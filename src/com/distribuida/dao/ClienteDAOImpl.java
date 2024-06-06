e49b8ff1671034b76775ba7d967430795f297b92
NOCTURNO <NOCTURNO@DESKTOP-6DM3JKP>
2024-06-05 19:41:26 -0500

    ELv0007 Principal Cliente

--- src/com/distribuida/dao/ClienteDAOImpl.java
+++ src/com/distribuida/dao/ClienteDAOImpl.java
@@ -7,9 +7,12 @@ import javax.transaction.Transactional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.distribuida.entities.Cliente;
 
+
+@Repository
 public class ClienteDAOImpl implements ClienteDAO {
 	
 	
@@ -28,7 +31,7 @@ public class ClienteDAOImpl implements ClienteDAO {
 	public List<Cliente> findAll() {
 		// TODO Auto-generated method stub
 		Session session = sessionFactory.getCurrentSession();
-		return session.createQuery("",Cliente.class).getResultList();
+		return session.createQuery("from Cliente",Cliente.class).getResultList();
 	}
 
 	@Override
